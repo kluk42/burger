188f4d60b1e667bbbf75b4cd32d4c565264c2b84
Немыкин Никита Валерьевич <nikita.nemykin@rt.ru>
2025-02-13 14:20:51 +0700

    add some more

--- src/__tests__/BurgerBuilder.test.tsx
+++ src/__tests__/BurgerBuilder.test.tsx
@@ -1,15 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { queryAllByAttribute, render, screen } from '@testing-library/react';
 import Burger from '../components/Burger';
+import { Ingredients } from '../hooks/useIngredients/types';
 
 describe('Tests for Burger component', () => {
   test('burger is displayed - no ingredients selected - should display empty burger sign', () => {
-    render(
+    const container = render(
       <Burger ingredients={{ Bacon: 0, Cheese: 0, Meat: 0, Salad: 0, Seeds1: 1, Seeds2: 1 }} />
     );
-    const helloElement = screen.getByText('Please start adding ingredients');
+    const helloElement = container.getByText('Please start adding ingredients');
 
     expect(helloElement).toBeTruthy();
   });
 
-  test('burger is displayed - cheese, meat and ham are selected - all selected ingredients are displayed and no empty sign', () => {});
+  test('burger is displayed - 2 cheese, 1 meat and 1 bacon are selected - all selected ingredients are displayed and no empty sign', () => {
+    const container = render(
+      <Burger ingredients={{ Bacon: 1, Cheese: 2, Meat: 1, Salad: 0, Seeds1: 1, Seeds2: 1 }} />
+    );
+
+    const cheeseElements = queryAllByAttribute('className', rendered., Ingredients.Cheese)
+  });
 });
